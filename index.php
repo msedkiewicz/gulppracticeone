82e477a4793e7b58276a0e1a84f8d65a5ea0555e
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-06 00:35:26 +0100

    Add author display code

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ if(have_posts()) {
             <a href="<?php echo get_permalink(); ?>">
                 <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
             </a>
+            By <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>">
+                <?php echo get_the_author_meta('display_name'); ?>
+            </a>
         </div>
     <?php }
  } else { ?>
