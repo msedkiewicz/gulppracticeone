9d0e3d087ad00bc23da4afbd4e3a577813c40ad8
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-06 00:27:58 +0100

    Add basic posts info

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php get_header(); ?>
-<h1>Lorem ipsum</h1>
-<?php get_footer(); ?>
+<?php get_header();
+
+if(have_posts()) {
+    while(have_posts()) {
+        the_post(); ?>
+        <h2>
+            <a href="<?php echo the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+        </h2>
+        <div>
+            Posted on:
+            <a href="<?php echo get_permalink(); ?>">
+                <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date('l, F j, Y'); ?></time>
+            </a>
+        </div>
+    <?php }
+ } else { ?>
+    <p>Sorry, no posts matched your criteria.</p>
+<?php }
+
+get_footer(); ?>
