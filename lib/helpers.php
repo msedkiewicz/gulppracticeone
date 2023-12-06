51c10f292c7eca994d7295e7581ea53cd9e17c26
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-06 17:17:40 +0100

    Add esc functions (security)

--- lib/helpers.php
+++ lib/helpers.php
@@ -5,14 +5,14 @@
 
 function gulpwp_meta_description() {
     echo 'Posted on: ';
-    echo '<a href="' . get_permalink() .'">';
-    echo '<time datetime="' . get_the_date('c') . '">' . get_the_date() . '</time>';
+    echo '<a href="' . esc_url(get_permalink()) .'">';
+    echo '<time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date()) . '</time>';
     echo '</a> ';
-    echo 'By <a href="' . get_author_posts_url(get_the_author_meta( 'ID' )) . '">' . get_the_author_meta('display_name') . '</a>';
+    echo 'By <a href="' . esc_url(get_author_posts_url(get_the_author_meta( 'ID' ))) . '">' . esc_html(get_the_author_meta('display_name')) . '</a>';
 }
 
 function gulpwp_readmore() {
-    echo '<a href="' . get_the_permalink() . '" title="' . the_title_attribute(['echo' => false]) . '">';
+    echo '<a href="' . esc_url(get_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
     echo 'Read more <span class="u-screen-reader-text">About ' . the_title() . '</span>';
     echo '</a>';
 }
