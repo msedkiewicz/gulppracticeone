41cdcaaa5026af4a8089d84d54576de7c35b644e
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-06 19:45:58 +0100

    Add translation option for read more

--- lib/helpers.php
+++ lib/helpers.php
@@ -11,13 +11,23 @@ function gulpwp_meta_description() {
         );
 
     printf(
-        esc_html( ' By %s', 'gulppracticeone' ),
+        esc_html__( ' By %s', 'gulppracticeone' ),
         '<a href="' . esc_url(get_author_posts_url(get_the_author_meta( 'ID' ))) . '">' . esc_html(get_the_author_meta('display_name')) . '</a>'
         );
 }
 
 function gulpwp_readmore() {
     echo '<a href="' . esc_url(get_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
-    echo 'Read more <span class="u-screen-reader-text">About ' . the_title() . '</span>';
+    printf(
+        wp_kses(
+            __('Read more <span class="u-screen-reader-text">About %s</span>', 'gulppracticeone' ),
+            [
+                'span' => [
+                    'class' => []
+                ]
+            ]
+        ),
+        get_the_title()
+    );
     echo '</a>';
 }
