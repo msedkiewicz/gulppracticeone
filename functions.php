2f8df25138f22b2ee261e9b2689c018a52ada90b
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-14 22:45:05 +0100

    Basic gulp configuration

--- functions.php
+++ functions.php
@@ -3,3 +3,9 @@
 
 /* Import helpers */
 require_once('lib/helpers.php');
+
+function theme_scripts() {
+  wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/dist/css/app.min.css', array(), wp_get_theme()->get( 'Version' )  );
+  wp_enqueue_script( 'theme-scripts', get_template_directory_uri() . '/dist/js/app.min.js', array(), false, true );
+}
+add_action( 'wp_enqueue_scripts', 'theme_scripts' );
